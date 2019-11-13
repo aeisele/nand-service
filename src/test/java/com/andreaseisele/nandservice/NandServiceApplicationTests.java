99b7490670b20aae05dab4bd75b27576007afd69
ben.horsfield <ben.horsfield@external.wirecard.com>
2019-11-13 14:50:31 +0100

    Add unit test

--- src/test/java/com/andreaseisele/nandservice/NandServiceApplicationTests.java
+++ src/test/java/com/andreaseisele/nandservice/NandServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package com.andreaseisele.nandservice;
 
+import com.andreaseisele.nandservice.controller.NandController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class NandServiceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testNAND() {
+        NandController controller = new NandController();
+
+        assertTrue(controller.nand(false, false));
+        assertTrue(controller.nand(true, false));
+        assertTrue(controller.nand(false, true));
+        assertFalse(controller.nand(true, true));
     }
 
 }
